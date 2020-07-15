101cd4a6357cc35b4778c7b542bbef171c73f8a2
Mauricio de Assis <mauriciox8mm@hotmail.com>
2020-07-14 22:19:07 -0300

    aula 8 Blade templates

--- curso/resources/views/contato/index.blade.php
+++ curso/resources/views/contato/index.blade.php
@@ -1,9 +1,15 @@
-<h1> essa é a view index de contatos</h1>
-sintax de objeto 
-@foreach ($contatos2 as $contato)
-    <h5>{{$contato->nome}}</h5>
-@endforeach
-sintax de array 
-@foreach ($contatos as $contato)
-    <h5>{{$contato['nome']}}</h5>
-@endforeach
+@extends('layout.site')
+
+@section('titulo', 'Contatos')
+
+@section('conteudo')
+    <h1> essa é a view index de contatos</h1>
+    sintax de objeto 
+    @foreach ($contatos2 as $contato)
+        <h5>{{$contato->nome}}</h5>
+    @endforeach
+    sintax de array 
+    @foreach ($contatos as $contato)
+        <h5>{{$contato['nome']}}</h5>
+    @endforeach
+@endsection
