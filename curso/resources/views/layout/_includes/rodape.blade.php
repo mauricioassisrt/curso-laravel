3440f54ac556a9c0268182b54690dec04830db83
Mauricio de Assis <mauriciox8mm@hotmail.com>
2020-07-21 20:23:47 -0300

    16. Formulário para adicionar cursos

--- curso/resources/views/layout/_includes/rodape.blade.php
+++ curso/resources/views/layout/_includes/rodape.blade.php
@@ -1,6 +1,6 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <!--JavaScript at end of body for optimized loading-->
-    <script type="text/javascript" src="../js/materialize.min.js"></script>
+    <script type="text/javascript" src="../../js/materialize.min.js"></script>
      <!-- Compiled and minified JavaScript -->
      {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> --}}
             
--- curso/resources/views/layout/_includes/topo.blade.php
+++ curso/resources/views/layout/_includes/topo.blade.php
@@ -8,7 +8,7 @@
     <!-- Compiled and minified CSS -->
     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css"> --}}
 
-    <link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="../../css/materialize.min.css"  media="screen,projection"/>
     <title>@yield('titulo')</title>
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
